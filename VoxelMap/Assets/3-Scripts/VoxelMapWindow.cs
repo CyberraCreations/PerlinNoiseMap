e46eafd1d681ab1d25c0a8e95c79ecb0488c143c
Cyberra <cyberracreations@gmail.com>
2016-12-25 15:41:44 -0500

    Create window

---
+++ VoxelMap/Assets/3-Scripts/VoxelMapWindow.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+public class VoxelMapWindow : EditorWindow
+{
+    #region Fields
+    #endregion
+    
+    #region Methods
+    private void OnEnable()
+    {
+        EditorApplication.update += RefreshOnPlay;
+    }
+
+    private void OnDisable()
+    {
+        EditorApplication.update -= RefreshOnPlay;
+    }
+
+    private void RefreshOnPlay()
+    {
+        if (EditorApplication.isPlaying)
+        {
+            Repaint();
+        }
+    }
+
+    [MenuItem("Window/VoxelMap")]
+    static void Init()
+    {
+        VoxelMapWindow mapCreator = GetWindow<VoxelMapWindow>();
+        GUIContent title = new GUIContent();
+        title.text = "VoxelMap";
+        mapCreator.titleContent = title;
+    }
+    #endregion
+}
